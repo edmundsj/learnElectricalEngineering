fc840b18214b3f4f3e42bc4d37c13394e3d92522
edmundsj <jordan.e@berkeley.edu>
2020-04-19 09:50:53 -0700

    added footer, begin/end functions for pages

---
+++ includes/functions.php
@@ -0,0 +1,13 @@
+<?php
+
+function beginPage() {
+	echo 'PHP is working!';
+	echo '<body class="is-preload">';
+	echo '<div id="wrapper">';
+}
+
+function endPage() {
+	echo '</div>';
+	echo '</body>';
+	echo '</html>';
+}
