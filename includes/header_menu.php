3baf95f5f96d2fb7f955974cb319b72fee4d8b9a
edmundsj <jordan.e@berkeley.edu>
2020-04-19 09:27:51 -0700

    my first commit

---
+++ includes/header_menu.php
@@ -0,0 +1,27 @@
+<!-- Header -->
+<header id="header">
+	<div class="inner">
+
+			<a href="index.html" class="logo">
+				<span class="symbol"><img src="images/logo.svg" alt="" /></span><span class="title                                    ">Phantom</span>
+			</a>
+
+			<nav>
+				<ul>
+					<li><a href="#menu">Menu</a></li>
+				</ul>
+			</nav>
+
+	</div>
+</header>
+
+<nav id="menu">
+	<h2>Menu</h2>
+	<ul>
+		<li><a href="index.html">Home</a></li>
+		<li><a href="generic.html">Ipsum veroeros</a></li>
+		<li><a href="generic.html">Tempus etiam</a></li>
+		<li><a href="generic.html">Consequat dolor</a></li>
+		<li><a href="elements.html">Elements</a></li>
+	</ul>
+</nav>
